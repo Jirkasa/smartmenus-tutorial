dcaff72467ffaa1bd596a2737a72d2474841c244
Jiří <satorajirka@seznam.cz>
2021-10-23 22:33:47 +0200

    write third part of tutorial

---
+++ media/js/parts/nastaveni-tykajici-se-interakce-s-menu.js
@@ -0,0 +1,19 @@
+$('#menu-1').smartmenus({
+    showOnClick: true
+});
+$('#menu-2').smartmenus({
+    showTimeout: 1000
+});
+$('#menu-3').smartmenus({
+    hideTimeout: 2000
+});
+$('#menu-4').smartmenus({
+    noMouseOver: true
+});
+$('#menu-5').smartmenus({
+    noMouseOver: true,
+    hideOnClick: false
+});
+$('#menu-6').smartmenus({
+    collapsibleBehavior: "toggle"
+});
