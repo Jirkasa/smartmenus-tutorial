a345d6fca0369aa03fced1f61a0524e01a886156
Jiří <satorajirka@seznam.cz>
2021-10-24 14:57:02 +0200

    write fifth part of tutorial

---
+++ media/js/parts/nastaveni-tykajici-se-sub-menu.js
@@ -0,0 +1,25 @@
+$('#menu-1').smartmenus({
+    bottomToTopSubMenus: true
+});
+$('#menu-2').smartmenus({
+    mainMenuSubOffsetX: 30
+});
+$('#menu-3').smartmenus({
+    mainMenuSubOffsetY: 40
+});
+$('#menu-4').smartmenus({
+    subMenusSubOffsetX: -10
+});
+$('#menu-5').smartmenus({
+    subMenusSubOffsetX: 20
+});
+$('#menu-6').smartmenus({
+    subMenusMinWidth: "0",
+    subMenusMaxWidth: "6rem"
+});
+$('#menu-7').smartmenus({
+    subMenusMinWidth: "20rem"
+});
+$('#menu-8').smartmenus({
+    rightToLeftSubMenus: true
+});
